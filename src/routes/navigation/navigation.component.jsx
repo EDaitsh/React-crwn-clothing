93609e248a7c24c8e719ecf8cd49e5ec9aebff19
edaitsh <e.daitsh@gmail.com>
2023-04-16 11:19:05 +0300

    Add Cart Icon & Dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,13 +3,19 @@ import {Outlet, Link} from 'react-router-dom';
 
 import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
 
+import CartIcon from '../../components/cart-icon/cart-icon.component'
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
 import {UserContext} from '../../contexts/user.context'
+import { CartContext } from '../../contexts/cart.context';
+
 import {signOutUser} from '../../utils/firebase/firebase.utils'
 
 import './navigation.styles.scss';
 
 const Navigation =() => {
     const {currentUser} = useContext(UserContext);
+    const {isCartOpen} = useContext(CartContext);
 
     console.log(currentUser);
     return (
@@ -34,8 +40,10 @@ const Navigation =() => {
                         SIGNIN
                     </Link>
                 )}
+                <CartIcon />
                 
             </div>
+            {isCartOpen && <CartDropdown/>}
         </div>
         <Outlet/>
       </Fragment>
