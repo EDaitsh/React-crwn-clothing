d5919453e0550363003cf5dccdc9593d774a873b
edaitsh <e.daitsh@gmail.com>
2023-04-27 09:54:51 +0300

    Logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,26 +1,15 @@
 import {compose,  applyMiddleware} from 'redux';
-import {persistStore, petsistReducer} from 'redux-persist';
+import {persistStore} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 //import {createStore } from "redux"
 import { legacy_createStore as createStore } from "redux";
-//import logger from 'redux-logger';
+import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 import persistReducer from 'redux-persist/es/persistReducer';
 //const middleWares = [logger];
 
-const loggerMiddleware = (store) => (next) => (action) =>{
-    if(!action.type){
-        return next(action);
-    }
-    console.log('type: ', action.type);
-    console.log('action: ', action.payload);
-    console.log('currentState: ', store.getState());
 
-    next(action);
-
-    console.log('next state: ', store.getState());
-}
 
 const persistConfig = {
     key: 'root',
@@ -30,7 +19,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
+const middleWares = [process.env.NODE_ENV!== 'production' && logger].filter(Boolean);
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
