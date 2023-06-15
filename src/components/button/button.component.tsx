a5bbedf0132b9d7c81da81506b1a554ef4fbb461
edaitsh <e.daitsh@gmail.com>
2023-06-15 15:11:55 +0300

    Convert Button component to type-script

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,16 +1,18 @@
+import { FC, ButtonHTMLAttributes } from 'react';
+
 import {BaseButton,
         GoogleSignInButton,
         InvertedButton,
         ButtonSpinner
 } from './button.styles'
 
-export const BUTTON_TYPE_CLASSES = {
-    base: 'base',
-    google: 'google-sign-in',
-    inverted: 'inverted'
+export enum BUTTON_TYPE_CLASSES {
+    base= 'base',
+    google= 'google-sign-in',
+    inverted= 'inverted'
 }
 
-const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => 
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base): typeof BaseButton => 
 ({
     [BUTTON_TYPE_CLASSES.base]: BaseButton,
     [BUTTON_TYPE_CLASSES.google] : GoogleSignInButton,
@@ -18,8 +20,12 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
 }[buttonType]);
 
 
+export type ButtonProps = {
+    buttonType?: BUTTON_TYPE_CLASSES;
+    isLoading?: boolean;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button =({children, buttonType, isLoading, ...otherProps}) => {
+const Button: FC<ButtonProps> = ({children, buttonType, isLoading, ...otherProps}) => {
     const CustomButton = getButton(buttonType);
     //console.log(buttonType);
     return (
